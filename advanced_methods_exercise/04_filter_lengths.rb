8b770b7b1d36ffe7365a0e1c0d72450156739d7b
SamanAtashi <milady00126@gmail.com>
2021-05-16 09:55:42 +0300

    Revise solution for Filter_lengths

--- advanced_methods_exercise/04_filter_lengths.rb
+++ advanced_methods_exercise/04_filter_lengths.rb
@@ -4,9 +4,7 @@
 # is passed in, then 5 should be used as the length.
 
 def filter_lengths(strings, len=5)
-    strings.select do |item|
-        item.size >= len
-    end
+     strings.select { |item| item.size >= len }
 end
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
