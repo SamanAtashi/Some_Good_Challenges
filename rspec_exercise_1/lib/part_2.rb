d5abe30cf58e3db6c33349b0e777402e74be0fbb
SamanAtashi <milady00126@gmail.com>
2021-05-20 12:53:17 +0300

    Add Final solutions

--- rspec_exercise_1/lib/part_2.rb
+++ rspec_exercise_1/lib/part_2.rb
@@ -1,16 +1,28 @@
+# def hipsterfy(word)
+#     i=word.length-1
+#     vowels="aeoiu"
+#     while i>=0
+#         # p word[i]
+#         # p vowels.include?(word[i])
+#         if vowels.include?(word[i])
+#             word[i] = ""
+#             break
+#         end
+#         i-=1
+#     end
+#     return word
+# end
+
 def hipsterfy(word)
     i=word.length-1
     vowels="aeoiu"
     while i>=0
-        # p word[i]
-        # p vowels.include?(word[i])
         if vowels.include?(word[i])
-            word[i] = ""
-            break
+            return word[0...i] + word[i+1..-1]
         end
         i-=1
     end
-    return word
+    word
 end
 
 def vowel_counts(str)
@@ -21,28 +33,45 @@ def vowel_counts(str)
     new_hash
 end
 
-def caesar_cipher(s, k)
-	alpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    alpha_s="ABCDEFGHIJKLMNOPQRSTUVWXYZ".downcase
-    # make the alphas rotate
-    new_alpha = alpha.split("")
-    new_alpha_s = alpha_s.split("")
-    k.times { |char| new_alpha.unshift(new_alpha.pop) }
-    k.times { |char| new_alpha_s.unshift(new_alpha_s.pop) }
-    
-    new_str = ""
-    s.each_char do |char , idx|
-        if new_alpha.include?(char)
-           new_str+=alpha[new_alpha.index(char)]
-        elsif new_alpha_s.include?(char)
-            new_str+=alpha_s[new_alpha_s.index(char)]
+def caesar_cipher(msg, n)
+    alphabet = ("a".."z").to_a
+    new_msg = ""
+    msg.each_char do |char|
+        if alphabet.include?(char)
+            old_idx = alphabet.index(char)
+            new_idx = old_idx + n
+            new_msg += alphabet[new_idx%26]
+            # we know our array is less than 26 char so if we go beyond that we reset to the 0 by module (remainder)
         else
-            new_str+=char
+            new_msg+=char
         end
-
     end
-    new_str
+    new_msg
 end
+
+# def caesar_cipher(s, k)
+# 	alpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     
+#     alpha_s="ABCDEFGHIJKLMNOPQRSTUVWXYZ".downcase
+#     # make the alphas rotate
+#     new_alpha = alpha.split("")
+#     new_alpha_s = alpha_s.split("")
+#     k.times { |char| new_alpha.unshift(new_alpha.pop) }
+#     k.times { |char| new_alpha_s.unshift(new_alpha_s.pop) }
+    
+#     new_str = ""
+#     s.each_char do |char , idx|
+#         if new_alpha.include?(char)
+#            new_str+=alpha[new_alpha.index(char)]
+#         elsif new_alpha_s.include?(char)
+#             new_str+=alpha_s[new_alpha_s.index(char)]
+#         else
+#             new_str+=char
+#         end
+
+#     end
+#     new_str
+# end
 
 # def caesar_cipher(s, k)
 #   arr = s.chars.map do |char|
